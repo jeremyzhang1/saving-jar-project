843dc024ed0a3e5c7691e5cca7ea3c1d168a5cc7
laurenc8 <laurenc8822@gmail.com>
2021-01-07 10:50:01 -0500

    edit routing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import PageLogin from './pages/PageLogin';
 import PageHome from './pages/PageHome';
-import PageDashboard from './pages/PageDashboard';
 import PageGroups from './pages/PageGroups';
+import PageGoal from './pages/PageGoal';
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={PageLogin} />
         <Route exact path="/home" component={PageHome} />
-        <Route exact path="/dashboard" component={PageDashboard} />
         <Route exact path="/groups" component={PageGroups} />
+        <Route exact path="/goal" component={PageGoal} />
         <Route component={() => <Redirect to="/" />} />
       </Switch>
     </div>
