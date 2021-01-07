a0caea8f9e77c795421aac21d12fe34efc830b53
Kimberly Gao <kimberlygao@Kimberlys-MacBook-Pro-2.local>
2021-01-07 15:44:10 -0600

    dashboard pig progress

--- src/components/progressPigs/DashboardPig.js
+++ src/components/progressPigs/DashboardPig.js
@@ -1,22 +1,20 @@
 //import { render } from "@testing-library/react";
 import React from "react";
 import ProgressBar from "./ProgressBar.js";
+import pigImage from "../../assets/pig.png"
 
 //const testData = { bgcolor: "#6a1b9a", completed: 60 };
 
 const DashboardPig = (props) => {
   const { bgcolor, total, current, completed} = props;
+
+  
+
+
     return(
         <div>
-          <h1> </h1>
-          <p> {props.completed}%</p>
-          <p> Completed</p>
-
-          <p> ${props.current} saved</p>
-          
-          <p> ${props.total} to go</p>
-
-        <ProgressBar bgcolor={props.bgcolor} completed={props.completed} />
+          <h1 class="align-left"> Disneyland </h1>
+          <ProgressBar bgcolor={props.bgcolor} completed={props.completed} />
     </div>
     )
 
--- src/components/progressPigs/ProgressBar.js
+++ src/components/progressPigs/ProgressBar.js
@@ -1,5 +1,6 @@
 //sets up the progress bar to be used oh the dashboard and the groups page  
 import React from "react";
+import pigImage from "../../assets/pig.png";
 
 //copied from https://dev.to/ramonak/react-how-to-create-a-custom-progress-bar-component-in-5-minutes-2lcl TODO: EDIT
 const ProgressBar = (props) => {
@@ -10,7 +11,10 @@ const ProgressBar = (props) => {
     width: '100%',
     backgroundColor: "#e0e0de",
     borderRadius: 50,
-    margin: 50
+    marginLeft: 50,
+    marginRight: 50,
+    marginTop: '-10%',
+    marginBottom: '10%'
   }
 
   const fillerStyles = {
@@ -19,6 +23,7 @@ const ProgressBar = (props) => {
     backgroundColor: bgcolor,
     borderRadius: 'inherit',
     textAlign: 'right'
+    
   }
 
   const labelStyles = {
@@ -27,10 +32,52 @@ const ProgressBar = (props) => {
     fontWeight: 'bold'
   }
 
+  const pigImgStyles = {
+    width: '100%'
+  }
+
+  const pigStyles = {
+    width: '30%',
+    position: 'relative',
+    textAlign: 'center',
+    color: 'white',
+    left: `${completed-10}%`
+  }
+
+  const compStyles = {
+    position: 'absolute',
+    textAlign: 'center',
+    top: '37%',
+    left: '33%'
+    
+  }
+
+  const savedStyles = {
+    position: 'absolute',
+    textAlign: 'center',
+    top: '47%',
+    left: '33%'
+  }
+
+  const totalStyles = {
+    position: 'absolute',
+    textAlign: 'center',
+    top: '57%',
+    left: '33%'
+  }
+
   return (
-    <div style={containerStyles}>
-      <div style={fillerStyles}>
-        <span style={labelStyles}>{`${completed}%`}</span>
+    <div>
+      <div class="container pig-stats" style={pigStyles}>
+              <img src={pigImage} style={pigImgStyles}/>
+              <p class="centered" style={compStyles}> {props.completed}% complete </p>
+              <p class="centered" style={savedStyles}> ${props.current} saved </p>
+              <p class="centered" style={totalStyles}> ${props.total} to go </p>
+      </div>
+      <div style={containerStyles}>
+        <div style={fillerStyles}>
+          <span style={labelStyles}>{`${completed}%`}</span>
+        </div>
       </div>
     </div>
   );
