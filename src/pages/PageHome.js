974bf51da6578fb8d3b0efb0522329e3957ab01c
qucarolyn <qu.carolyn@gmail.com>
2021-01-07 11:59:51 -0500

    pig progress tracker

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,7 +1,27 @@
+import React from "react";
+import DashboardPig from "../components/progressPigs/DashboardPig.js";
+
+const testData =
+  { bgcolor: "#6a1b9a", 
+  total: 100, 
+  current: 60, 
+  completed: 60,
+  startdate:'', 
+  enddate:'',
+};
+
+// data to include: total amount, percentage, 
+
 function PageHome() {
   return (
     <div>
         Home
+        <DashboardPig myPigTest 
+          bgcolor = {testData.bgcolor} 
+          current = {testData.current}
+          total = {testData.total} 
+          completed = {testData.completed}>
+          </DashboardPig>
     </div>
   );
 }
