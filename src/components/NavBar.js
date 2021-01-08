59c72d22b585d695ba3f81798596427e7f75c926
qucarolyn <qu.carolyn@gmail.com>
2021-01-08 01:20:49 -0500

    login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,8 +11,15 @@ import PageHome from '../pages/PageHome';
 import PageGroups from '../pages/PageGroups';
 import PageGoal from '../pages/PageGoal';
 import "./Navbar.css"
+import Button from 'react-bootstrap/Button';
+import fire from "../config/fire";
+
 
 class BootstrapNavbar extends React.Component{
+
+    logout(){
+        fire.auth().signOut();
+    }
 
     render(){
         return(
@@ -21,10 +28,11 @@ class BootstrapNavbar extends React.Component{
                     <Navbar bg="dark" variant="pills" expand="lg" sticky="top" style={{position: "absolute", width: "100%"}}>
                         <Nav className='navigation' class="navbar navbar-expand-md navbar-fixed-top">
 
-                            <Nav.Link href="/login">Logout</Nav.Link> 
+                            <Nav.Link onSelect={this.logout} href="/login">Logout</Nav.Link> 
                             <Nav.Link id="ghome" href="/home"  >Home</Nav.Link>
                             <Nav.Link id="bgroup" href="/groups" >Find a Group</Nav.Link>
                             <Nav.Link href="/goal">Goal Page</Nav.Link>
+                            {/* <Button bsStyle="primary">Login</Button> */}
                         </Nav>
                     </Navbar>
                             <br />
